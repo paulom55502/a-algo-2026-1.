244c5125492891f144498b342f45cb0912636cb5
paulom55502 <128054384+paulom55502@users.noreply.github.com>
2026-04-09 17:16:47 -0300

    Update main.py

--- deveres-de-casa/dever-03/main.py
+++ deveres-de-casa/dever-03/main.py
@@ -1,1 +1,12 @@
 
+def eh_palindromo(arr, esq=0, dir=None):
+   
+    if dir is None:
+        dir = len(arr) - 1
+
+    if esq >= dir:
+        return True
+    if arr[esq] != arr[dir]:
+        return False
+
+    return eh_palindromo(arr, esq + 1, dir - 1)
